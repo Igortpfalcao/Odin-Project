937171cf47e5adbcb9cc14a182ff7f3efb628a3e
igortpfalcao <rogianilorac@gmail.com>
2024-09-27 11:12:50 -0300

    project done

--- rock paper scissors/rockpaperscissors.js
+++ rock paper scissors/rockpaperscissors.js
@@ -1,55 +1,105 @@
 console.log("Let's play a funny game of rock paper scissors!");
 
+var humanScore = 0;
+var computerScore = 0;
+
+playgame();
+
 function getComputerChoice(){
-    let N = (math.floor(math.random() * 3) + 1);
+    let N = (Math.floor(Math.random() * 3) + 1);
 
     switch (N){
         
         case 1:
             return "Rock";
-            break;
-        
+      
         case 2:
             return "Paper";
-            break;
         
         case 3:
             return "Scissors"
-            break;
     }
 }
 
 function getHumanChoice(){
-    let N = prompt("Choose your move: 1 = rock, 2 = paper, 3 = scissors  (invalid anwers result in the move being chosen at random!)");
+    let N = parseInt(prompt("Choose your move: 1 = rock, 2 = paper, 3 = scissors  (invalid anwers result in the move being chosen at random!)"));
     if (N != 1 && N != 2 && N != 3){
-        N = (math.floor(math.random() * 3) + 1);
+        N = (Math.floor(Math.random() * 3) + 1);
+        console.log("Invalid choice! A random move has been selected.");
     } 
 
     switch (N){
         
         case 1:
             return "Rock";
-            break;
-        
+                 
         case 2:
             return "Paper";
-            break;
         
         case 3:
-            return "Scissors"
-            break;
+            return "Scissors"           
     }
 }
 
-function Main(){
+function playround(){
     const H = getHumanChoice();
     const C = getComputerChoice();
-    console.log("You chose" + H + " and the Computer chose" + C +".");
+    console.log("You chose " + H + " and the Computer chose " + C +".");
     if (H === C){
         console.log("It's a draw!")
+        return;
     }
-    if (H === "Paper"){
-        
+    else if (H === "Paper"){
+        if (C === "Rock"){
+            console.log("Congrats! You've won!");
+            humanScore += 1;
+            return;
+        }
+        else{
+            console.log("You lose...");
+            computerScore += 1;
+            return;
+        }
     }
+    else if (H === "Rock"){
+        if (C === "Scissors"){
+            console.log("Congrats! You've won!");
+            humanScore += 1;
+            return;
+        }
+        else{
+            console.log("You lose...");
+            computerScore += 1;
+            return;
+        }
+    }
+    else{
+        if (C === "Paper"){
+            console.log("Congrats! You've won!");
+            humanScore += 1;
+            return;
+        }
+        else{
+            console.log("You lose...");
+            computerScore += 1;
+            return;
+        }
+    }
+}
 
+function playgame(){
+    playround();
+    playround();
+    playround();
+    playround();
+    playround();
+    if (humanScore === computerScore){
+        console.log("It's a draw! Final score: " + humanScore + " x " + computerScore + "!");
+    }
+    else if (humanScore >= computerScore){
+        console.log("Congrats! you have won! The final score was " + humanScore + " x " + computerScore + "!");
+    }
+    else{
+        console.log("I'm sorry... You lost. Final score: " + humanScore + " x " + computerScore + "!");
+    }
 }
