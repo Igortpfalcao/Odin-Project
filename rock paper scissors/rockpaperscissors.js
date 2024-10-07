cf225f76c4857af06d583d630e927a542e4b9217
igortpfalcao <rogianilorac@gmail.com>
2024-10-07 12:05:18 -0300

    starting to revisit the rps project to add an ui to it

--- rock paper scissors/rockpaperscissors.js
+++ rock paper scissors/rockpaperscissors.js
@@ -1,9 +1,23 @@
-console.log("Let's play a funny game of rock paper scissors!");
-
 var humanScore = 0;
 var computerScore = 0;
 
-playgame();
+const message = document.querySelector("message");
+
+const result = document.querySelector("result");
+result.style.cssText = "display: flex; justify-content: center; font-size: 18px; font-weight: 700; margin-top: 80px;" 
+
+const choiceRock = document.querySelector("rock");
+
+const choicePaper = document.querySelector("paper");
+
+const choiceScissors = document.querySelector("scissors");
+
+choiceRock.addEventListener("click", playround("Rock"));
+
+choicePaper.addEventListener("click", playround("Paper"));
+
+choiceScissors.addEventListener("click", playround("Scissors"));
+
 
 function getComputerChoice(){
     let N = (Math.floor(Math.random() * 3) + 1);
@@ -21,49 +35,28 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let N = parseInt(prompt("Choose your move: 1 = rock, 2 = paper, 3 = scissors  (invalid anwers result in the move being chosen at random!)"));
-    if (N != 1 && N != 2 && N != 3){
-        N = (Math.floor(Math.random() * 3) + 1);
-        console.log("Invalid choice! A random move has been selected.");
-    } 
-
-    switch (N){
-        
-        case 1:
-            return "Rock";
-                 
-        case 2:
-            return "Paper";
-        
-        case 3:
-            return "Scissors"           
-    }
-}
-
-function playround(){
-    const H = getHumanChoice();
+function playround(input){
     const C = getComputerChoice();
-    console.log("You chose " + H + " and the Computer chose " + C +".");
-    if (H === C){
-        console.log("It's a draw!")
+    message.textContent = "You chose" + input + "and the computer chose" + C + ".";
+    if (input === C){
+        result.textContent = "It's a draw."
         return;
     }
-    else if (H === "Paper"){
+    else if (input === "Paper"){
         if (C === "Rock"){
-            console.log("Congrats! You've won!");
+            result.textContent = "Congrats! You've won!";
             humanScore += 1;
             return;
         }
         else{
-            console.log("You lose...");
+            result.textContent = "You lose...";
             computerScore += 1;
             return;
         }
     }
-    else if (H === "Rock"){
+    else if (input === "Rock"){
         if (C === "Scissors"){
-            console.log("Congrats! You've won!");
+            result.textContent = "Congrats! You've won!";
             humanScore += 1;
             return;
         }
@@ -87,19 +80,3 @@ function playround(){
     }
 }
 
-function playgame(){
-    playround();
-    playround();
-    playround();
-    playround();
-    playround();
-    if (humanScore === computerScore){
-        console.log("It's a draw! Final score: " + humanScore + " x " + computerScore + "!");
-    }
-    else if (humanScore >= computerScore){
-        console.log("Congrats! you have won! The final score was " + humanScore + " x " + computerScore + "!");
-    }
-    else{
-        console.log("I'm sorry... You lost. Final score: " + humanScore + " x " + computerScore + "!");
-    }
-}
